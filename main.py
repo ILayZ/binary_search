0a40bc611f160c016555cc61b249e16179168f83
ILayZ <i.a.zaharchuk@gmail.com>
2019-06-23 18:17:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ def binary_search(sorted_arr, x):
         mid = (lo + hi) // 2        
         if sorted_arr[mid] < x: lo = mid + 1
         else: hi = mid
-        print( "lo = " + str(lo) + " hi = " + str(hi) + " mid = " + str(mid) )
+        #print( "lo = " + str(lo) + " hi = " + str(hi) + " mid = " + str(mid) )
     return lo
 
 array = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
 print( binary_search(array, 15) )
+print( binary_search(array, 70) )
+print( binary_search(array, -100) )
+print( binary_search(array, 0) )
+print( binary_search(array, 110) )
